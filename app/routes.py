60306802c74928b2768a8ed883812b2ff4ccf745
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-05-18 11:18:05 +0200

    Db added

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 from flask import render_template
-from app import app
+from app import app, db
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    u = User(username='susan', email='susan@example.com')
+    db.session.add(u)
+    db.session.commit()
+
     user = {'username': 'Jacek'}
     return render_template('index.html', title='Home', user=user)
