822de9c7910ac81aeee2e4b77dbf4db204ece231
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-05-18 15:31:51 +0200

    Flash notifications added

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app, db
 from app.models import User
 
@@ -9,5 +9,7 @@ def index():
     # u = User(username='susan', email='susan@example.com')
     # db.session.add(u)
     # db.session.commit()
-
+    flash('Example success notification!', 'success')
+    flash('Example error notification!', 'error')
+    flash('Example warning notification!', 'warning!')
     return render_template('index.html')
--- config.py
+++ config.py
@@ -7,3 +7,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'very-secret-key'
