0276eb48c39bcb25b9dff99d4e29a4c53735f1c8
rpanimation <rpanimation@icloud.com>
2021-05-20 10:13:04 +0200

    Added FontAwesome library.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template, flash
 from app import app, db
 from app.models import User
+from flask_fontawesome import FontAwesome
 
+
+fa = FontAwesome(app)
 
 @app.route('/')
 @app.route('/index')
