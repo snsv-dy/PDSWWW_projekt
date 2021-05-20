4174ef3485cf5f4c44dd4c409ea76f991d5fed24
Jacek Stasiak <snsvgb@wp.pl>
2021-05-20 17:08:16 +0200

    eh

--- app/routes.py
+++ app/routes.py
@@ -13,3 +13,57 @@ def index():
     flash('Example error notification!', 'error')
     flash('Example warning notification!', 'warning!')
     return render_template('index.html')
+
+@app.route('/test/<int:index>')
+def quiz(index=1):
+	questions = [
+		{
+			'index': 1,
+			'type': 0,
+			'content': 'aaa',
+			'anwsers': [
+				'a',
+				'b'
+			],
+			'images': [
+				'baked_salmon.webp',
+				'jupiter.jpg'
+			],
+			'points': 2
+		},
+		{
+			'index': 2,
+			'type': 1,
+			'content': 'Co jest cięższe?',
+			'anwsers': [
+				'1kg piór',
+				'1kg stali'
+			],
+			'points': 2
+		},
+		{
+			'index': 3,
+			'type': 2,
+			'content': 'Chjian',
+			'images': [
+				'jupiter.jpg'
+			],
+			'points': 2
+		}
+	]
+
+	test_params = {
+		'title': 'tytuł',
+		'author': 'autor',
+		'questions': questions,
+		'number_of_questions': len(questions)
+	}
+
+	index -= 1
+
+	if index < 0 or index >= len(questions):
+		index = 0
+
+	question = questions[index]
+
+	return render_template('test.html', test_params=test_params, question=question)
