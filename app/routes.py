d0757b1d6d3fcc116d4164d2daacb77ddb767777
Jacek Stasiak <snsvgb@wp.pl>
2021-05-20 19:57:23 +0200

    Added reviewing view

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash
 from app import app, db
 from app.models import User
+from app.sample_data import sample_test, sample_anwsers
 
 
 @app.route('/')
@@ -14,106 +15,28 @@ def index():
     flash('Example warning notification!', 'warning!')
     return render_template('index.html')
 
-
-edited_test = {
-	'title': 'tytuł',
-	'author': 'autor',
-	'questions': [
-		{
-			'index': 1,
-			'type': 0,
-			'content': 'aaa',
-			'anwsers': [
-				'a',
-				'b'
-			],
-			'images': [
-				'baked_salmon.webp',
-				'jupiter.jpg'
-			],
-			'points': 2
-		},
-		{
-			'index': 2,
-			'type': 1,
-			'content': 'Co jest cięższe?',
-			'anwsers': [
-				'1kg piór',
-				'1kg stali'
-			],
-			'points': 2
-		},
-		{
-			'index': 3,
-			'type': 2,
-			'content': 'Chjian',
-			'images': [
-				'jupiter.jpg'
-			],
-			'points': 2
-		}
-	],
-	'number_of_questions': 3
-}
+@app.route('/test_review')
+def quiz_review():
+	return render_template('test_review.html', test_params=sample_anwsers)
 
 @app.route('/test_edit/<int:index>')
 def quiz_edit(index=1):
 
 	index -= 1
 
-	questions = edited_test['questions']
+	questions = sample_test['questions']
 
 	if index < 0 or index >= len(questions):
 		index = 0
 
 	question = questions[index]
 
-	return render_template('test_edit.html', test_params=edited_test, question=question)
+	return render_template('test_edit.html', test_params=sample_test, question=question)
 
 @app.route('/test/<int:index>')
 def quiz(index=1):
-	questions = [
-		{
-			'index': 1,
-			'type': 0,
-			'content': 'aaa',
-			'anwsers': [
-				'a',
-				'b'
-			],
-			'images': [
-				'baked_salmon.webp',
-				'jupiter.jpg'
-			],
-			'points': 2
-		},
-		{
-			'index': 2,
-			'type': 1,
-			'content': 'Co jest cięższe?',
-			'anwsers': [
-				'1kg piór',
-				'1kg stali'
-			],
-			'points': 2
-		},
-		{
-			'index': 3,
-			'type': 2,
-			'content': 'Chjian',
-			'images': [
-				'jupiter.jpg'
-			],
-			'points': 2
-		}
-	]
 
-	test_params = {
-		'title': 'tytuł',
-		'author': 'autor',
-		'questions': questions,
-		'number_of_questions': len(questions)
-	}
+	questions = sample_test['questions']
 
 	index -= 1
 
@@ -122,4 +45,4 @@ def quiz(index=1):
 
 	question = questions[index]
 
-	return render_template('test.html', test_params=test_params, question=question)
+	return render_template('test.html', test_params=sample_test, question=question)
