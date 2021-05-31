6afa03f1a2b720ef3ee1783b4dfa1c2123faefdd
Jacek Stasiak <snsvgb@wp.pl>
2021-05-31 18:16:56 +0200

    Saving test anwsers when switching between questions

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
@@ -76,8 +76,31 @@ def quiz_edit(number=1):
     return render_template('test_edit.html', test_params=sample_test, question=question)
 
 
-@app.route('/test/<int:number>')
+question_anwsers = [
+	[1, 3], 2, 'Siema'
+]
+
+def update_previous_question(form):
+    question_type = form.get('question_type')
+    question_index = form.get('question_index')
+
+    anwser = form.getlist('anwser')
+    index = int(question_index) - 1
+
+    print(anwser, index)
+
+    if question_type == '0':
+        question_anwsers[index] = [int(i) for i in anwser]
+    elif question_type == '1':
+        question_anwsers[index] = int(anwser[0])
+    else:
+        question_anwsers[index] = anwser[0]
+
+@app.route('/test/<int:number>', methods=['GET', 'POST'])
 def quiz(number=1):
+    if len(request.form) > 0:
+        update_previous_question(request.form)
+
     questions = sample_test['questions']
     number -= 1
 
@@ -85,7 +108,8 @@ def quiz(number=1):
         number = 0
 
     question = questions[number]
-    return render_template('test.html', test_params=sample_test, question=question)
+    anwser = question_anwsers[number]
+    return render_template('test.html', test_params=sample_test, question=question, anwsers=anwser)
 
 
 @app.route('/test_summary')
