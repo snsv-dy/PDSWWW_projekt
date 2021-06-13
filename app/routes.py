1b83a1371324d073311759d3cc038e293d3c4e36
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-06-13 09:34:37 +0200

    Nice error pages added

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,22 @@ from app.models import *
 from app.util import display_form_errors
 from app.email import send_email
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', message='Żądana strona nie istnieje'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('error.html', message='Wystąpił błąd serwera'), 500
+
+
+@app.errorhandler(401)
+def page_not_found(e):
+    return render_template('error.html', message='Brak autoryzacjia'), 401
+
+
 @app.route('/')
 @app.route('/index')
 def index():
