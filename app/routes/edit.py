16262440c45ac6a817d189d706c0d6eacc2e8f11
Jacek Stasiak <snsvgb@wp.pl>
2021-06-18 16:53:08 +0200

    Adding and removing images

--- app/routes/edit.py
+++ app/routes/edit.py
@@ -5,10 +5,10 @@ from app.models import *
 
 
 # @app.route('/edit/structure/<action>/<param>', methods=['GET', 'POST'])
-def quiz_edit_structure(action, param, test_obj):
+def quiz_edit_structure(action, param, test_obj, img_url=None):
     print('quiz edit', test_obj)
     if action == 'add_question' and param in ['0', '1', '2']:
-        question = Question(type=int(param), question='', points=1.0, data=None, nr=len(test_obj.questions))
+        question = Question(type=int(param), question='', points=1.0, data=None, nr=len(test_obj.questions), image=[])
 
         if param in ['0', '1']:
             question.data = {'all': [''], 'correct': [] if param == '0' else -1}
@@ -30,6 +30,40 @@ def quiz_edit_structure(action, param, test_obj):
         db.session.add(test_obj)
         db.session.commit()
         return redirect(f'/edit/{test_obj.id}/{index}')
+    elif action == 'remove_image':
+        print('param', param)
+        spit = param.split(';')
+        param = {'image_index': int(spit[1]), 'question_number': int(spit[0])}
+        image_i = param['image_index']
+        question = test_obj.questions[param['question_number']]
+        if image_i >= len(question.image):
+            flash('Błędny indeks zdjęcia.', 'error')
+        else:
+            new_list = [i + '' for i in question.image]
+            del new_list[image_i]
+            question.image = new_list
+
+            db.session.add(question)
+            db.session.add(test_obj)
+            db.session.commit()
+
+        return redirect(f'/edit/{test_obj.id}/{param["question_number"]}')
+    elif action == 'add_image':
+        if img_url is None:
+            flash('Brak url nowego zdjęcia', 'error')
+        else:
+            question = test_obj.questions[int(param)]
+            new_list = [i + '' for i in question.image]
+            new_list.append(img_url)
+            question.image = new_list
+
+            db.session.add(question)
+            db.session.add(test_obj)
+            db.session.commit()
+
+        return redirect(f'/edit/{test_obj.id}/{int(param)}')
+        # return redirect(f'/edit/{test_obj.id}/1')
+
 
 def update_test_question(form, test_obj):
     print(form)
@@ -88,13 +122,15 @@ def quiz_edit(test_id=None, number=None, action=None, param=None):
 
     editable = session.get('editing_id')
     if not editable:
-        return redirect(url_for('test_preview', test_id=test.id))
+        session['editing_id'] = 1
+        editable = 1
+        # return redirect(url_for('test_preview', test_id=test.id))
 
-    if len(request.form) > 0 and editable:
+    if len(request.form) > 0 and 'new_img_url' not in request.form:
         update_test_question(request.form, test)
 
     if action is not None and param is not None and editable:
-        return quiz_edit_structure(action, param, test)
+        return quiz_edit_structure(action, param, test, img_url=request.form.get('new_img_url'))
 
     if number is None:
         return redirect(f'/edit/{test_id}/1')
@@ -108,6 +144,7 @@ def quiz_edit(test_id=None, number=None, action=None, param=None):
     question = None
     if len(questions) > 0:
         question = questions[number]
+        print('image', question.image)
 
     if not editable:
         flash('Tryb podglądu, zmiany nie zostaną zapisane.', 'warning')
