994a2e6aa49a7b7609e77e56948108b3cf5e4695
rpanimation <rpanimation@icloud.com>
2021-06-20 11:58:38 +0200

    Added test import/export.

--- app/routes/manage.py
+++ app/routes/manage.py
@@ -1,4 +1,8 @@
-from flask import render_template, flash, redirect, url_for, session
+import json
+from json import JSONEncoder
+
+import pdb
+from flask import render_template, flash, redirect, url_for, session, make_response, request
 from flask_login import login_required, current_user
 from app import app
 from app.models import *
@@ -135,20 +139,71 @@ def finish_term(term_id):
 def export_test(test_id):
     check_res = check_test(test_id)
     if check_res is not None: return check_res
+    test = Test.query.filter_by(id=test_id).first()
 
-    # TODO: Implement exporting test
+    class ExportedQuestion:
+        def __init__(self, number, question, type, answers, correct, points, images):
+            self.number = number
+            self.question = question
+            self.type = type
+            self.answers = answers
+            self.correct = correct
+            self.points = points
+            self.images = images
 
-    flash('Pomyślnie wyeksportowano test', 'success')
-    return redirect(url_for('details', test_id=test_id))
+    class ExportedTest:
+        def __init__(self, title, questions, time):
+            self.time = time
+            self.title = title
+            self.questions = questions
+
+    exp_list = []
+    for question in test.questions:
+        if  question.type == Question.MULTIPLE_CHOICE:
+            exp_quest = ExportedQuestion(question.nr, question.question, question.type, question.data['all'], question.data['correct'], question.points, question.image)
+            exp_list.append(exp_quest)
+        elif question.type == Question.SINGLE_CHOICE:
+            exp_quest = ExportedQuestion(question.nr, question.question, question.type, question.data['all'], [question.data['correct']], question.points, question.image)
+            exp_list.append(exp_quest)
+        elif question.type == Question.OPEN:
+            exp_quest = ExportedQuestion(question.nr, question.question, question.type, [], [], question.points, question.image)
+            exp_list.append(exp_quest)
+
+    exp_test = ExportedTest(test.title, exp_list, test.time)
+    file = json.dumps(exp_test, default=lambda o: o.__dict__, indent=4)
+
+    response = make_response(file)
+    response.headers.set('Content-Type', 'text/plain')
+    response.headers.set('Content-Disposition', 'attachment', filename='%s.json'%test.title)
+    return response
 
 
-@app.route('/import')
+
+@app.route('/upload')
+@login_required
+def upload_test():
+    return render_template('upload_file.html')
+
+
+@app.route('/import', methods = ['GET', 'POST'])
 @login_required
 def import_test():
-
-    # TODO: Implement importing test
-
-    flash('Pomyślnie zaimportowano test', 'success')
+    teacher = current_user
+    def validate_json(file):
+        # TODO: Implement file validation
+        return True
+    if request.method == 'POST':
+        f = request.files['file']
+        json_data = json.load(f)
+        if (validate_json(json_data)):
+            test = Test(title=json_data['title'], time=json_data['time'])
+            teacher.tests.append(test)
+            db.session.add(test)
+            for quesiton in json_data['questions']:
+                quest = Question(nr=quesiton['number'], question=quesiton['question'], points=quesiton['points'], image=quesiton['images'], type=quesiton['type'], data={'all': quesiton['answers'], 'correct': quesiton['correct']})
+                test.questions.append(quest)
+                db.session.add(quest)
+            db.session.commit()
     return redirect(url_for('manage'))
 
 
